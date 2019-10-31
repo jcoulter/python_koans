1dc588e4e334540ae8bbdef021f3ca69649dcbd4
Rachel Coulter <rachelcoulter13@gmail.com>
2019-10-31 12:37:03 -0400

    Fixing Asserts and Strings

--- python3/koans/about_strings.py
+++ python3/koans/about_strings.py
@@ -8,23 +8,23 @@ class AboutStrings(Koan):
 
     def test_double_quoted_strings_are_strings(self):
         string = "Hello, world."
-        self.assertEqual("Hello world", isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
     def test_single_quoted_strings_are_also_strings(self):
         string = 'Goodbye, world.'
-        self.assertEqual('Goodbye, world.', isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
     def test_triple_quote_strings_are_also_strings(self):
         string = """Howdy, world!"""
-        self.assertEqual("""Howdy, world!""", isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
     def test_triple_single_quotes_work_too(self):
         string = '''Bonjour tout le monde!'''
-        self.assertEqual('''Bonjour tout le monde!''', isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
     def test_raw_strings_are_also_strings(self):
         string = r"Konnichi wa, world!"
-        self.assertEqual(r"Konnichi wa, world!", isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
     def test_use_single_quotes_to_create_string_with_double_quotes(self):
         string = 'He said, "Go Away."'
@@ -42,18 +42,14 @@ class AboutStrings(Koan):
     def test_use_backslash_at_the_end_of_a_line_to_continue_onto_the_next_line(self):
         string = "It was the best of times,\n\
 It was the worst of times."
-        self.assertEqual("It was the best of times,\n\
-It was the worst of times.", len(string))
+        self.assertEqual(52, len(string))
 
     def test_triple_quoted_strings_can_span_lines(self):
         string = """
 Howdy,
 world!
 """
-        self.assertEqual("""
-Howdy,
-world!
-""", len(string))
+        self.assertEqual(15, len(string))
 
     def test_triple_quoted_strings_need_less_escaping(self):
         a = "Hello \"world\"."
@@ -82,7 +78,7 @@ world!
         hi = "Hello, "
         there = "world"
         hi += there
-        self.assertEqual("Hello, ", "world", hi)
+        self.assertEqual("Hello, world", hi)
 
     def test_plus_equals_also_leaves_original_string_unmodified(self):
         original = "Hello, "
@@ -95,4 +91,4 @@ world!
         string = "\n"
         self.assertEqual('\n', string)
         self.assertEqual("""\n""", string)
-        self.assertEqual("\n", len(string))
+        self.assertEqual(1, len(string))
